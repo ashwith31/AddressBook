51ed78ce6aaeb8da6977c6fb9b6d803cb23e0aa6
ashwith31 <ashwitash@gmail.com>
2021-08-12 11:46:42 +0530

    Welcome message

---
+++ src/com/addressBook/AddressBookMain.java
@@ -0,0 +1,15 @@
+package com.addressBook;
+
+/*
+ *  Program to create Address Book System. 
+ * @author Ashwith
+ */
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Address Book Program");
+		
+	}
+
+}
